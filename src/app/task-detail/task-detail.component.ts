7ae943e841148faf5e6c20a4e5cab3f23477a0f7
Jarrett Sorensen <khunicycler@hotmail.com>
2023-01-15 16:11:29 -0700

    build: fix errors

--- src/app/task-detail/task-detail.component.ts
+++ src/app/task-detail/task-detail.component.ts
@@ -8,7 +8,7 @@ import { TaskService }  from '../task.service';
 @Component({
   selector: 'app-task-detail',
   templateUrl: './task-detail.component.html',
-  styleUrls: [ './task-detail.component.css' ]
+  styleUrls: [ './task-detail.component.scss' ]
 })
 export class TaskDetailComponent implements OnInit {
   @Input() task: Task;
